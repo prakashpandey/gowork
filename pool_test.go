232f243eacb57cc74a31cec2012c9fbed2a240b8
Prakash Pandey <prakashpandeyy@yahoo.com>
2025-06-18 23:09:58 +0530

    test: Added error and panic recovery test cases.

--- pool_test.go
+++ pool_test.go
@@ -2,6 +2,7 @@ package gowork
 
 import (
 	"context"
+	"errors"
 	"sync/atomic"
 	"testing"
 
@@ -24,4 +25,42 @@ func TestSuccessfulExecution(t *testing.T) {
 
 	assert.EqualValues(t, 10, counter)
 	assert.EqualValues(t, 0, len(pool.Errors()))
+}
+
+func TestErrorCollections(t *testing.T) {
+	numWorkers := 50
+	task := Task{
+		Fn: func(ctx context.Context) error {
+			return errors.New("task failed")
+		},
+	}
+
+	pool := NewPool(context.Background(), numWorkers, task)
+	pool.Go()
+	pool.Wait()
+
+	errs := pool.Errors()
+	assert.EqualValues(t, numWorkers, len(errs))
+
+	workerIDs := make(map[int]bool)
+	for _, e := range errs {
+		// should not have duplicate worker id.
+		assert.Equal(t, false, workerIDs[e.WorkerID])
+		workerIDs[e.WorkerID] = true
+	}
+	assert.Equal(t, numWorkers, len(workerIDs))
+}
+
+func TestPanicRecovery(t *testing.T) {
+	panicMsg := "worker panicked"
+	task := Task{
+		Fn: func(ctx context.Context) error {
+			panic(panicMsg)
+		},
+	}
+	pool := NewPool(context.Background(), 1, task)
+	pool.Go()
+	pool.Wait()
+
+	assert.Contains(t, pool.Errors()[0].Error(), panicMsg)
 }
